16689d9b96c7dd81d144076c56fbdc591d9c4789
AlexO <alexandreosorio18@gmail.com>
2024-01-13 17:18:59 +1100

    some progress

--- routes/skills.js
+++ routes/skills.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/javascript', function(req, res, next) {
+  res.render('index', { title: 'JavaScript' });
+});
+
+router.get('/react', function(req, res, next) {
+  res.render('index', { title: 'React' });
+});
+
 module.exports = router;
